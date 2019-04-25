098676f9f35ac3e418cc7b3f076032904db5bb36
ivannz <ivannz@yandex.ru>
2019-04-25 09:09:54 +0300

    setup sciprt for installation

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup, Extension
+
+setup(
+    name="cplxmodule",
+    version="0.1",
+    description="""A lightweight extension for pytorch.nn for handling """
+                """complex valued computations.""",
+    license="MIT License",
+    author="Ivan Nazarov",
+    author_email="ivan.nazarov@skolkovotech.ru",
+    packages=["cplxmodule"],
+    requires=["torch", "numpy"]
+)
