1983e55fa76c6436eaecb66cc527dfb0e0684924
ivannz <ivan.nazarov@skolkovotech.ru>
2019-05-20 15:36:15 +0300

    TST minor improvements of welch implementation in torch

--- tests/draw_welch.py
+++ tests/draw_welch.py
@@ -2,8 +2,11 @@ import torch
 import numpy as np
 import matplotlib.pyplot as plt
 
-from cplxmodule.spectrum import pwelch, fftshift
+from cplxmodule.utils.spectrum import pwelch, fftshift
 from scipy.signal import welch
+
+
+from matplotlib.ticker import EngFormatter
 
 random_state = np.random.RandomState(479321083)
 
@@ -11,13 +14,12 @@ random_state = np.random.RandomState(479321083)
 fs = 1000.
 tt = np.r_[:5 * fs - 1] / fs
 
-np_x = 1j * random_state.randn(2, len(tt)) * .01
-np_x += random_state.randn(2, len(tt)) * .01
+shape = 2, len(tt)
 
-np_x += np.cos(2 * np.pi * 100 * tt)[np.newaxis]
-np_x = np_x.astype(np.complex)
+epsilon = random_state.randn(*shape) + 1j * random_state.randn(*shape)
+np_x = np.cos(2 * np.pi * 100 * tt)[np.newaxis] + epsilon * 0.01
 
-tr_x = torch.tensor(np_x.view(np.float).reshape(*np_x.shape, 2))
+tr_x = torch.tensor(np.stack([np_x.real, np_x.imag], axis=-1))
 tr_x.requires_grad = False
 
 tr_window = torch.hamming_window(500, periodic=False, dtype=tr_x.dtype)
@@ -28,10 +30,20 @@ np_ff, np_px = welch(np_x, fs=fs, axis=-1, window=tr_window.numpy(),
                      nfft=None, nperseg=None, scaling="density",
                      noverlap=300, detrend=False, return_onesided=False)
 
-plt.semilogy(np.fft.fftshift(np_ff), np.fft.fftshift(np_px[0]),
-             label="scipy", alpha=0.5)
-plt.semilogy(fftshift(tr_ff).numpy(), fftshift(tr_px[0]).numpy().T,
-             label="torch", alpha=0.5)
-plt.legend()
+fig = plt.figure(figsize=(14, 5))
+
+ax = fig.add_subplot(111, ylabel="Decibels (dB / Hz)", xlabel="Frequency")
+ax.xaxis.set_major_formatter(EngFormatter(unit="Hz"))
+
+ax.semilogy(np.fft.fftshift(np_ff), np.fft.fftshift(np_px[0]),
+            label="scipy", alpha=0.5)
+
+ax.semilogy(fftshift(tr_ff).numpy(), fftshift(tr_px[0]).numpy().T,
+            label="torch", alpha=0.5)
+
+ax.axvspan(90, 110, alpha=0.25, color="green")
+ax.axvline(100, lw=2, linestyle=":", color="black", alpha=0.5)
+
+ax.legend()
 
 plt.show()
