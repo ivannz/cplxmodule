585599de4bcc1f339794c367c73230cea0c862f8
ivannz <ivannz@yandex.ru>
2019-05-26 01:17:32 +0300

    ModReLU activation layerwith a learnable threshold per dim

--- cplxmodule/activation.py
+++ cplxmodule/activation.py
@@ -56,6 +56,34 @@ class CplxModReLU(CplxToCplx):
         return cplx_modrelu(input, self.threshold)
 
 
+class CplxAdaptiveModReLU(CplxToCplx):
+    r"""
+    Applies soft thresholding to the complex modulus:
+    $$
+        F
+        \colon \mathbb{C}^d \to \mathbb{C}^d
+        \colon z \mapsto (\lvert z_j \rvert - \tau_j)_+
+                        \tfrac{z_j}{\lvert z_j \rvert}
+        \,, $$
+    with $\tau_j \in \mathbb{R}$ being the $j$-th learnable threshold. Torch's
+    broadcasting rules apply and the passed dimensions must conform with the
+    upstream input. `CplxChanneledModReLU(1)` learns a common threshold for all
+    features of the $d$-dim complex vector, and `CplxChanneledModReLU(d)` lets
+    each dimension have its own threshold.
+    """
+    def __init__(self, *dim):
+        super().__init__()
+        self.dim = dim if dim else (1,)
+        self.threshold = torch.nn.Parameter(torch.randn(*self.dim) * 0.02)
+
+    def forward(self, input):
+        return cplx_modrelu(input, self.threshold)
+
+    def __repr__(self):
+        body = repr(self.dim)[1:-1] if len(self.dim) > 1 else repr(self.dim[0])
+        return f"{self.__class__.__name__}({body})"
+
+
 class CplxModulus(CplxToReal):
     def forward(self, input):
         return abs(input)
