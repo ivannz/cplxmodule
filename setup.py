f3530f33ec7c83e298d784bb636efb75987c2969
ivannz <ivannz@yandex.ru>
2019-06-17 23:09:23 +0300

    version bump and submodule declaration

--- setup.py
+++ setup.py
@@ -2,12 +2,17 @@ from distutils.core import setup, Extension
 
 setup(
     name="cplxmodule",
-    version="0.6.1",
+    version="0.6.5",
     description="""A lightweight extension for pytorch.nn for handling """
                 """complex valued computations.""",
     license="MIT License",
     author="Ivan Nazarov",
     author_email="ivan.nazarov@skolkovotech.ru",
-    packages=["cplxmodule", "cplxmodule.utils", "cplxmodule.relevance"],
+    packages=[
+        "cplxmodule",
+        "cplxmodule.utils",
+        "cplxmodule.relevance",
+        "cplxmodule.masked"
+    ],
     requires=["torch", "numpy"]
 )
