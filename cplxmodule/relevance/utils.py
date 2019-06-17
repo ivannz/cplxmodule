48c326906d59fffefaf0a6a8954f27e3dd499c67
Ivan <ivannz@yandex.ru>
2019-06-17 21:14:26 +0300

    Completely removed sparse-mode support from relevance

--- cplxmodule/relevance/utils.py
+++ cplxmodule/relevance/utils.py
@@ -6,12 +6,6 @@ import torch.sparse
 
 import torch.nn.functional as F
 
-from ..masked.utils import torch_sparse_tensor
-from ..masked.utils import torch_sparse_linear
-from ..masked.utils import torch_sparse_cplx_linear
-
-from ..cplx import Cplx
-
 
 class ExpiFunction(torch.autograd.Function):
     r"""Pythonic differentiable port of scipy's Exponential Integral Ei.
@@ -75,80 +69,3 @@ def kldiv_approx(log_alpha, coef, reduction):
         return kl_div.sum()
 
     return kl_div
-
-
-def parameter_to_buffer(module, name):
-    if not hasattr(module, "stash_"):
-        module.stash_ = {}
-
-    assert isinstance(module.stash_, dict)
-
-    # par could be a solo parameter or a container (essentially a submodule)
-    par = getattr(module, name)
-    if isinstance(par, (torch.nn.ParameterDict, torch.nn.ParameterList)):
-        # parameter containers no not use buffers and aren't expected to.
-        #  So we hide parameters there. This precludes acces via __getitem__
-        #  though. Not via __getattr__
-
-        # create a copy of the container's master parameter dict's keys and mutate
-        for name in list(par._parameters):
-            # By design of Parameter containers this never recurses deeper
-            parameter_to_buffer(par, name)
-        return
-
-    # a solo parameter
-    if par is not None and not isinstance(par, torch.nn.Parameter):
-        raise KeyError(f"parameter '{name}' is not a tensor.")
-
-    if name in module.stash_:
-        raise RuntimeError(f"Parameter `{name}` has already been stashed.")
-
-    # remove the parameter and mutate into a grad-detached buffer
-    delattr(module, name)
-
-    # .detach_() detaches the tensor in-place (not just retaining storage, but
-    # also python object). requires_grad_() does the same, but is less obscure
-    # .detach() returns a new tensor with that never requires grad, but shares
-    # the storage with the original
-    buf = par.requires_grad_(False) if par is not None else None
-
-    # Buffers are not manipulated using in-place operations and thus devolve
-    # into torch.Tensor, losing reference to the original (out-of-place). Thus
-    # keep a shadow copy of the original parameter.
-    module.stash_[name] = par
-
-    # buffer is OK unless ".to()" is called, when it devolves into torch.Tensor
-    module.register_buffer(name, buf)
-
-
-def buffer_to_parameter(module, name):
-    if not hasattr(module, "stash_"):
-        module.stash_ = {}
-
-    assert isinstance(module.stash_, dict)
-
-    # a buffer here can be a buffer or a former mutated parameter container
-    buf = getattr(module, name)
-    if isinstance(buf, (torch.nn.ParameterDict, torch.nn.ParameterList)):
-        # create a copy of the container's master buffer dict's keys and restore
-        for name in list(buf._buffers):
-            # By design of Parameter containers this never goes deeper
-            #  than this call
-            buffer_to_parameter(buf, name)
-        return
-
-    if buf is not None and not isinstance(buf, torch.Tensor):
-        raise KeyError(f"buffer '{name}' is not a tensor.")
-
-    if name not in module.stash_:
-        raise RuntimeError(f"Parameter `{name}` has not been stashed.")
-
-    # remove the buffer and mutate back into a proper parameter
-    delattr(module, name)
-    par = module.stash_.pop(name)
-    if par is not None:
-        if buf is not None:
-            par.data = par.data.to(buf)
-        par.requires_grad_(True)
-
-    module.register_parameter(name, par)
