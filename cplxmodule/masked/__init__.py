5b74391877847828d6205078c85633c37e79e348
Ivan <ivannz@yandex.ru>
2019-06-18 01:13:32 +0300

    fixed missing import

--- cplxmodule/masked/__init__.py
+++ cplxmodule/masked/__init__.py
@@ -2,6 +2,7 @@ from .base import BaseMasked
 
 from .base import is_sparse, named_masks
 from .base import deploy_masks, compute_ard_masks
+from .base import load_masks_from_state_dict
 
 from .real import LinearMasked
 from .complex import CplxLinearMasked
