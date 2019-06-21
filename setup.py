1187726000cafab24b0733e5a76ad70f7cb120d9
ivannz <ivan.nazarov@skolkovotech.ru>
2019-06-21 17:48:28 +0300

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup, Extension
 
 setup(
     name="cplxmodule",
-    version="0.6.5",
+    version="0.6.8",
     description="""A lightweight extension for pytorch.nn for handling """
                 """complex valued computations.""",
     license="MIT License",
