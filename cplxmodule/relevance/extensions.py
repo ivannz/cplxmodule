367015e06ee446679644663d7f4cb9e28f5d6679
ivannz <ivannz@yandex.ru>
2019-08-10 13:37:11 +0300

    Added the ard model from arxiv:1811.00596

--- cplxmodule/relevance/extensions.py
+++ cplxmodule/relevance/extensions.py
@@ -1,7 +1,42 @@
 import torch
 import torch.nn.functional as F
 
+from .real import LinearARD as LinearVD
 from .complex import ExpiFunction, CplxLinearARD
+
+
+class LinearARD(LinearVD):
+    @property
+    def penalty(self):
+        r"""Penalty from arxiv:1811.00596.
+
+        Naming
+        ------
+        In fact this is the true ARD setup, and the all layers in other
+        files are in fact ordinary Variational Dropout. The difference
+        is in the prior.
+
+        Notes
+        -----
+        Computes the KL-divergence of $q_\theta(W)$ from $p(W; \tau)$ in
+        the (true) ARD setup, suggested in arxiv:1811.00596. Uses mean
+        field gaussian variational approximation $q_\theta(W)$ against
+        the assumed proper (mean field) prior $p(W; \tau)$ gaussian with
+        elementwise precision parameter $\tau$ optimised in the ELBO.
+        $$
+            \mathop{KL}\bigl(q_\theta(W) \| p(W)\bigr)
+                = \mathbb{E}_{W \sim q_\theta}
+                    \log \tfrac{q_\theta(W)}{p(W)}
+                = \frac12 \sum_{ij} \bigl(
+                    \tau_{ij} \sigma^2_{ij} + \tau_{ij} \mu_{ij}^2
+                    - \log \sigma^2_{ij} - \log \tau_{ij} - 1
+                \bigr)
+            \,, $$
+        at $\tau_{ij} = (\sigma^2_{ij} + \mu_{ij}^2)^{-1}$.
+        """
+
+        # `softplus` is $x \mapsto \log(1 + e^x)$
+        return 0.5 * F.softplus(- self.log_alpha)
 
 
 class CplxLinearARDApprox(CplxLinearARD):
