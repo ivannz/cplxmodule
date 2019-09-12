d51362d2a4338de2d23023a6a4f737a158b854cd
ivannz <ivan.nazarov@skolkovotech.ru>
2019-09-12 15:07:47 +0300

    Cplx-dropout with empirical prior

--- cplxmodule/relevance/extensions.py
+++ cplxmodule/relevance/extensions.py
@@ -40,6 +40,40 @@ class LinearARD(LinearVD):
         return 0.5 * F.softplus(- self.log_alpha)
 
 
+class CplxLinearARD(CplxLinearVD):
+    @property
+    def penalty(self):
+        r"""Empricial Bayes penalty for complex layer with complex gaussian vi.
+
+        Naming
+        ------
+        See `cplxmodule.relevance.extensions.LinearARD`
+
+        Notes
+        -----
+        Computes the KL-divergence of $q_\theta(W)$ from the emprical Bayes
+        prior $\pi(W; \tau)$ given by a circular symmetric complex gaussian
+        with optimal precision parameter $\tau$.
+        $$
+            \mathop{KL}\bigl(q_\theta(W) \| \pi(W; \tau_{ij} \bigr)
+                = \mathbb{E}_{W \sim q_\theta}
+                    \log \tfrac{q_\theta(W)}{\pi(W; \tau_{ij})}
+                = \sum_{ij} \bigl(
+                    \tau_{ij} \sigma^2_{ij}
+                    + \tau_{ij} \lvert \mu_{ij} \rvert^2
+                    - \log \sigma^2_{ij} \tau_{ij}
+                    - 1
+                \bigr)
+            \,, $$
+        at $\tau_{ij} = (\sigma^2_{ij} + \mu_{ij}^2)^{-1}$.
+
+        Note the absence of $\tfrac12$!
+        """
+
+        # `softplus` is $x \mapsto \log(1 + e^x)$
+        return F.softplus(- self.log_alpha)
+
+
 class CplxLinearVDApprox(CplxLinearVD):
     @property
     def penalty(self):
