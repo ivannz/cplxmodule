fixed inadequate 1e-20 const: fp32 ReLU death and -ve zeros in vardropout variance

cplxmodule/relevance/complex.py
@@ -208,7 +208,7 @@ class CplxConv1dARD(CplxConv1d, BaseARD, SparsityStats):
                       self.padding, self.dilation, self.groups)
 
         noise = Cplx(*map(torch.randn_like, (s2, s2))) / sqrt(2)
-        return mu + noise * torch.sqrt(s2 + 1e-20)
+        return mu + noise * torch.sqrt(s2 + 1e-8)
 
     relevance = CplxLinearARD.relevance
 
@@ -250,7 +250,7 @@ class CplxConv2dARD(CplxConv2d, BaseARD, SparsityStats):
                       self.padding, self.dilation, self.groups)
 
         noise = Cplx(*map(torch.randn_like, (s2, s2))) / sqrt(2)
-        return mu + noise * torch.sqrt(s2 + 1e-20)
+        return mu + noise * torch.sqrt(s2 + 1e-8)
 
     relevance = CplxLinearARD.relevance
 

cplxmodule/relevance/real.py
@@ -145,7 +145,7 @@ class Conv1dARD(torch.nn.Conv1d, BaseARD, SparsityStats):
 
         s2 = F.conv1d(input * input, torch.exp(self.log_sigma2), None,
                       self.stride, self.padding, self.dilation, self.groups)
-        return mu + torch.randn_like(s2) * torch.sqrt(s2 + 1e-20)
+        return mu + torch.randn_like(s2) * torch.sqrt(s2 + 1e-8)
 
     relevance = LinearARD.relevance
 