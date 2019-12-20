d58873bca4fb7e11d89ac0fee9c502654ae97792
ivannz <ivan.nazarov@skolkovotech.ru>
2019-12-20 19:06:47 +0300

    implemented intializers, proposed by Trabelsi et al.(2017)

--- cplxmodule/init.py
+++ cplxmodule/init.py
@@ -25,3 +25,79 @@ def cplx_xavier_normal_(tensor, gain=1.0):
 
     init.xavier_normal_(tensor.real, gain=gain/math.sqrt(2))
     init.xavier_normal_(tensor.imag, gain=gain/math.sqrt(2))
+
+
+def get_fans(cplxtensor):
+    """Almost verbatim copy of `init._calculate_fan_in_and_fan_out`"""
+    ndim = cplxtensor.dim()
+    if ndim < 2:
+        raise ValueError("Fan in and fan out can not be computed "
+                         "for tensor with fewer than 2 dimensions.")
+
+    n_fmaps_output, n_fmaps_input, *rest = cplxtensor.shape
+    if ndim == 2:
+        fan_in, fan_out = n_fmaps_output, n_fmaps_input
+
+    else:
+        receptive_field_size = np.prod((1, *rest))
+        fan_in = n_fmaps_input * receptive_field_size
+        fan_out = n_fmaps_output * receptive_field_size
+
+    return fan_in, fan_out
+
+
+def cplx_trabelsi_standard_(cplx, kind="glorot"):
+    """Standard complex initialization proposed in Trabelsi et al. (2017)."""
+    kind = kind.lower()
+    assert kind in ("glorot", "xavier", "kaiming", "he")
+
+    fan_in, fan_out = get_fans(cplx)
+    if kind == "glorot" or kind == "xavier":
+        scale = 1 / math.sqrt(fan_in + fan_out)
+    else:
+        scale = 1 / math.sqrt(fan_in)
+
+    # Rayleigh(\sigma / \sqrt2) x uniform[-\pi, +\pi] on p. 7
+    rho = np.random.rayleigh(scale, size=cplx.shape)
+    theta = np.random.uniform(-np.pi, +np.pi, size=cplx.shape)
+
+    # eq. (8) on p. 6
+    with torch.no_grad():
+        cplx.real.copy_(torch.from_numpy(np.cos(theta) * rho))
+        cplx.imag.copy_(torch.from_numpy(np.sin(theta) * rho))
+
+    return cplx
+
+
+def cplx_trabelsi_independent_(cplx, kind="glorot"):
+    """Orthogonal complex initialization proposed in Trabelsi et al. (2017)."""
+    kind = kind.lower()
+    assert kind in ("glorot", "xavier", "kaiming", "he")
+
+    ndim = cplx.dim()
+    if ndim == 2:
+        shape = cplx.shape
+
+    else:
+        shape = np.prod(cplx.shape[:2]), np.prod(cplx.shape[2:])
+
+    # generate a semi- unitary (orthogonal) matrix from a random matrix
+    # M = U V is semi-unitary: V^H U^H U V = I_k
+    Z = np.random.randn(*shape) + 1j * np.random.randn(*shape)
+    u, _, vh = np.linalg.svd(Z, compute_uv=True, full_matrices=False)
+    k = min(*shape)
+    M = np.dot(u[:, :k], vh[:, :k].conjugate().T)
+
+    fan_in, fan_out = get_fans(cplx)
+    if kind == "glorot" or kind == "xavier":
+        scale = 1 / math.sqrt(fan_in + fan_out)
+    else:
+        scale = 1 / math.sqrt(fan_in)
+
+    M /= M.std() / scale
+    M = M.reshape(cplx.shape)
+    with torch.no_grad():
+        cplx.real.copy_(torch.from_numpy(M.real))
+        cplx.imag.copy_(torch.from_numpy(M.imag))
+
+    return cplx
