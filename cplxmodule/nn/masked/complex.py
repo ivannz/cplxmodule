b7297fc07014a4d7f05d5a97bd7dd894354513df
Ivan Nazarov <ivannz@yandex.ru>
2020-03-06 21:14:31 +0300

    moved masked-weight-mixin into the base mixin, since it does not affect the attribute lookup

--- cplxmodule/nn/masked/complex.py
+++ cplxmodule/nn/masked/complex.py
@@ -10,7 +10,7 @@ from .base import BaseMasked, MaskedWeightMixin
 from ..utils.sparsity import SparsityStats
 
 
-class _BaseCplxMixin(BaseMasked, SparsityStats):
+class _BaseCplxMixin(MaskedWeightMixin, BaseMasked, SparsityStats):
     def sparsity(self, *, hard=True, **kwargs):
         weight = self.weight
 
@@ -25,24 +25,24 @@ class _BaseCplxMixin(BaseMasked, SparsityStats):
         return [(id(weight.real), n_dropped), (id(weight.imag), n_dropped), ]
 
 
-class CplxLinearMasked(MaskedWeightMixin, CplxLinear, _BaseCplxMixin):
+class CplxLinearMasked(CplxLinear, _BaseCplxMixin):
     def forward(self, input):
         return cplx.linear(input, self.weight_masked, self.bias)
 
 
-class CplxBilinearMasked(MaskedWeightMixin, CplxBilinear, _BaseCplxMixin):
+class CplxBilinearMasked(CplxBilinear, _BaseCplxMixin):
     def forward(self, input1, input2):
         return cplx.bilinear(input1, input2, self.weight_masked, self.bias)
 
 
-class CplxConv1dMasked(MaskedWeightMixin, CplxConv1d, _BaseCplxMixin):
+class CplxConv1dMasked(CplxConv1d, _BaseCplxMixin):
     def forward(self, input):
         return cplx.conv1d(input, self.weight_masked, self.bias,
                            self.stride, self.padding, self.dilation,
                            self.groups, self.padding_mode)
 
 
-class CplxConv2dMasked(MaskedWeightMixin, CplxConv2d, _BaseCplxMixin):
+class CplxConv2dMasked(CplxConv2d, _BaseCplxMixin):
     def forward(self, input):
         return cplx.conv2d(input, self.weight_masked, self.bias,
                            self.stride, self.padding, self.dilation,
--- cplxmodule/nn/masked/real.py
+++ cplxmodule/nn/masked/real.py
@@ -7,7 +7,7 @@ from .base import BaseMasked, MaskedWeightMixin
 from ..utils.sparsity import SparsityStats
 
 
-class _BaseRealMixin(BaseMasked, SparsityStats):
+class _BaseRealMixin(MaskedWeightMixin, BaseMasked, SparsityStats):
     def sparsity(self, *, hard=True, **kwargs):
         if self.is_sparse:
             mask = torch.gt(self.mask, 0) if hard else self.mask
@@ -20,23 +20,23 @@ class _BaseRealMixin(BaseMasked, SparsityStats):
         return [(id(self.weight), n_dropped)]
 
 
-class LinearMasked(MaskedWeightMixin, Linear, _BaseRealMixin):
+class LinearMasked(Linear, _BaseRealMixin):
     def forward(self, input):
         return F.linear(input, self.weight_masked, self.bias)
 
 
-class Conv1dMasked(MaskedWeightMixin, Conv1d, _BaseRealMixin):
+class Conv1dMasked(Conv1d, _BaseRealMixin):
     def forward(self, input):
         return F.conv1d(input, self.weight_masked, self.bias,
                         self.stride, self.padding, self.dilation, self.groups)
 
 
-class Conv2dMasked(MaskedWeightMixin, Conv2d, _BaseRealMixin):
+class Conv2dMasked(Conv2d, _BaseRealMixin):
     def forward(self, input):
         return F.conv2d(input, self.weight_masked, self.bias,
                         self.stride, self.padding, self.dilation, self.groups)
 
 
-class BilinearMasked(MaskedWeightMixin, Bilinear, _BaseRealMixin):
+class BilinearMasked(Bilinear, _BaseRealMixin):
     def forward(self, input1, input2):
         return F.bilinear(input1, input2, self.weight_masked, self.bias)
