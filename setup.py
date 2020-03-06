d28aeced4436274fd77ee230a14a697aaa26a95e
Ivan Nazarov <ivannz@yandex.ru>
2020-03-06 21:24:31 +0300

    rewrote the description, and declared the `cplxmodule.nn.modules` submodule

--- setup.py
+++ setup.py
@@ -3,14 +3,18 @@ from distutils.core import setup, Extension
 setup(
     name="cplxmodule",
     version="0.9.7",
-    description="""A lightweight extension for pytorch.nn for handling """
-                """complex valued computations.""",
+    description="An extension for pytorch providing essential building blocks"
+                " for complex-valued networks and `torch.nn`-compatible"
+                " Bayesian sparsification methods. Implements the tools"
+                " necessary to train and sparsify both real and complex-valued"
+                " models, and seamlessly integrate them into existing models.",
     license="MIT License",
     author="Ivan Nazarov",
     author_email="ivan.nazarov@skolkovotech.ru",
     packages=[
         "cplxmodule",
         "cplxmodule.nn",
+        "cplxmodule.nn.modules",
         "cplxmodule.nn.relevance",
         "cplxmodule.nn.masked",
         "cplxmodule.nn.utils",
