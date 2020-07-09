c159ddc3abaaee7e455168d5e72c9d9578b3060d
ivannz <ivannz@yandex.ru>
2020-07-09 23:05:24 +0300

    expose R and C conv3d varaitional dropout layers

--- cplxmodule/nn/relevance/__init__.py
+++ cplxmodule/nn/relevance/__init__.py
@@ -2,15 +2,15 @@ from .base import penalties, named_penalties
 from .base import named_relevance, compute_ard_masks
 
 from .real import LinearVD, BilinearVD
-from .real import Conv1dVD, Conv2dVD
+from .real import Conv1dVD, Conv2dVD, Conv3dVD
 
 from .complex import CplxLinearVD, CplxBilinearVD
-from .complex import CplxConv1dVD, CplxConv2dVD
+from .complex import CplxConv1dVD, CplxConv2dVD, CplxConv3dVD
 
 from .ard import LinearARD, BilinearARD
-from .ard import Conv1dARD, Conv2dARD
+from .ard import Conv1dARD, Conv2dARD, Conv3dARD
 
 from .ard import CplxLinearARD, CplxBilinearARD
-from .ard import CplxConv1dARD, CplxConv2dARD
+from .ard import CplxConv1dARD, CplxConv2dARD, CplxConv3dARD
 
 from .extensions import LinearL0, LinearLASSO
