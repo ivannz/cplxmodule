f18fe6f3a15f83850c5ab5c27ff059d7fcd8d931
Ivan Nazarov <ivannz@yandex.ru>
2022-06-08 07:27:38 +0300

    FIX the missing dunder-version oversight pointed out in issue #24; bump to 2022.06

--- cplxmodule/__init__.py
+++ cplxmodule/__init__.py
@@ -1,4 +1,5 @@
 """Complex-valued modules for pytorch."""
+from .__version__ import __version__
 
 from .cplx import Cplx, from_real, to_real
 
--- setup.py
+++ setup.py
@@ -1,47 +1,48 @@
+import os
 from setuptools import setup
 
-setup(
-    name="cplxmodule",
-    version="2021.03.29a",
-    description="Variational Dropout and Complex-valued Neural Networks in pytorch",
-    long_description="An extension for pytorch providing essential building blocks "
-                     "for Complex-valued Neural Networks and `torch.nn`-compatible "
-                     "real- and complex-valued Variational Dropout methods. "
-                     "Implements the tools necessary to train and sparsify "
-                     "both real and complex-valued models, and seamlessly "
-                     "integrate them into existing pipelines.",
-    url="https://github.com/ivannz/cplxmodule",
-    keywords=[
-        "pytorch",
-        "variational dropout",
-        "complex-valued networks",
-        "sparsification"
-    ],
-    license="MIT License",
-    author="Ivan Nazarov",
-    author_email="ivan.nazarov@skolkovotech.ru",
-    packages=[
-        "cplxmodule",
-        "cplxmodule.nn",
-        "cplxmodule.nn.modules",
-        "cplxmodule.nn.relevance",
-        "cplxmodule.nn.relevance.real",
-        "cplxmodule.nn.relevance.complex",
-        "cplxmodule.nn.relevance.extensions",
-        "cplxmodule.nn.relevance.extensions.real",
-        "cplxmodule.nn.masked",
-        "cplxmodule.nn.utils",
-        "cplxmodule.utils",
-    ],
-    install_requires=[
-        "torch>=1.8",
-        "numpy",
-        "scipy"
-    ],
-    tests_require=[
-        "tqdm",
-        "torchvision",
-        "matplotlib",
-        "pytest"
-    ]
-)
+
+if __name__ == "__main__":
+    # update the version number from the file at the root
+    version = open("VERSION", "r").read().strip()
+
+    cwd = os.path.dirname(os.path.abspath(__file__))
+    with open(os.path.join(cwd, "cplxmodule", "__version__.py"), "w") as f:
+        f.write(f'__version__ = "{version}"\n')
+
+    setup(
+        name="cplxmodule",
+        version=version,
+        description="Variational Dropout and Complex-valued Neural Networks in pytorch",
+        long_description="""An extension for pytorch providing essential building blocks """
+        """for Complex-valued Neural Networks and `torch.nn`-compatible """
+        """real- and complex-valued Variational Dropout methods. """
+        """Implements the tools necessary to train and sparsify """
+        """both real and complex-valued models, and seamlessly """
+        """integrate them into existing pipelines.""",
+        url="https://github.com/ivannz/cplxmodule",
+        keywords=[
+            "pytorch",
+            "variational dropout",
+            "complex-valued networks",
+            "sparsification",
+        ],
+        license="MIT License",
+        author="Ivan Nazarov",
+        author_email="ivan.nazarov@skolkovotech.ru",
+        packages=[
+            "cplxmodule",
+            "cplxmodule.nn",
+            "cplxmodule.nn.modules",
+            "cplxmodule.nn.relevance",
+            "cplxmodule.nn.relevance.real",
+            "cplxmodule.nn.relevance.complex",
+            "cplxmodule.nn.relevance.extensions",
+            "cplxmodule.nn.relevance.extensions.real",
+            "cplxmodule.nn.masked",
+            "cplxmodule.nn.utils",
+            "cplxmodule.utils",
+        ],
+        install_requires=["torch>=1.8", "numpy", "scipy"],
+        tests_require=["tqdm", "torchvision", "matplotlib", "pytest"],
+    )
