045eebbe1424b5c73f95a9a5c0911eac36895f1c
Ivan Nazarov <ivannz@yandex.ru>
2022-06-08 13:08:22 +0300

    unleash black styler on .nn.init

--- cplxmodule/nn/init.py
+++ cplxmodule/nn/init.py
@@ -13,8 +13,10 @@ def get_fans(cplxtensor):
     """Almost verbatim copy of `init._calculate_fan_in_and_fan_out`"""
     ndim = cplxtensor.dim()
     if ndim < 2:
-        raise ValueError("Fan in and fan out can not be computed "
-                         "for tensor with fewer than 2 dimensions.")
+        raise ValueError(
+            "Fan in and fan out can not be computed for tensor with "
+            "fewer than 2 dimensions."
+        )
 
     n_fmaps_output, n_fmaps_input, *rest = cplxtensor.shape
     if ndim == 2:
@@ -29,7 +31,7 @@ def get_fans(cplxtensor):
 
 
 @wraps(init.kaiming_normal_, assigned=("__name__", "__doc__", "__annotations__"))
-def cplx_kaiming_normal_(tensor, a=0., mode='fan_in', nonlinearity='leaky_relu'):
+def cplx_kaiming_normal_(tensor, a=0.0, mode="fan_in", nonlinearity="leaky_relu"):
     assert isinstance(tensor, Cplx)
 
     a = math.sqrt(1 + 2 * a * a)
@@ -41,12 +43,12 @@ def cplx_kaiming_normal_(tensor, a=0., mode='fan_in', nonlinearity='leaky_relu')
 def cplx_xavier_normal_(tensor, gain=1.0):
     assert isinstance(tensor, Cplx)
 
-    init.xavier_normal_(tensor.real, gain=gain/math.sqrt(2))
-    init.xavier_normal_(tensor.imag, gain=gain/math.sqrt(2))
+    init.xavier_normal_(tensor.real, gain=gain / math.sqrt(2))
+    init.xavier_normal_(tensor.imag, gain=gain / math.sqrt(2))
 
 
 @wraps(init.kaiming_uniform_, assigned=("__name__", "__doc__", "__annotations__"))
-def cplx_kaiming_uniform_(tensor, a=0., mode='fan_in', nonlinearity='leaky_relu'):
+def cplx_kaiming_uniform_(tensor, a=0.0, mode="fan_in", nonlinearity="leaky_relu"):
     assert isinstance(tensor, Cplx)
 
     a = math.sqrt(1 + 2 * a * a)
@@ -58,8 +60,8 @@ def cplx_kaiming_uniform_(tensor, a=0., mode='fan_in', nonlinearity='leaky_relu'
 def cplx_xavier_uniform_(tensor, gain=1.0):
     assert isinstance(tensor, Cplx)
 
-    init.xavier_uniform_(tensor.real, gain=gain/math.sqrt(2))
-    init.xavier_uniform_(tensor.imag, gain=gain/math.sqrt(2))
+    init.xavier_uniform_(tensor.real, gain=gain / math.sqrt(2))
+    init.xavier_uniform_(tensor.imag, gain=gain / math.sqrt(2))
 
 
 def cplx_trabelsi_standard_(cplx, kind="glorot"):
@@ -102,8 +104,7 @@ def cplx_trabelsi_independent_(cplx, kind="glorot"):
     Z = np.random.rand(*shape) + 1j * np.random.rand(*shape)
 
     # Z is n x m, so u is n x n and vh is m x m
-    u, _, vh = np.linalg.svd(Z, compute_uv=True, full_matrices=True,
-                             hermitian=False)
+    u, _, vh = np.linalg.svd(Z, compute_uv=True, full_matrices=True, hermitian=False)
     k = min(*shape)
     M = np.dot(u[:, :k], vh[:, :k].conjugate().T)
 
@@ -122,7 +123,7 @@ def cplx_trabelsi_independent_(cplx, kind="glorot"):
     return cplx
 
 
-def cplx_uniform_independent_(cplx, a=0., b=1.):
+def cplx_uniform_independent_(cplx, a=0.0, b=1.0):
     torch.nn.init.uniform_(cplx.real, a, b)
     torch.nn.init.uniform_(cplx.imag, a, b)
 
